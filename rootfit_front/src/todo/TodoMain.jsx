2ba09a11a1227a06b3952d0607cfe8b29caaec49
jieun <j@naver.com>
2024-01-24 23:50:52 +0900

    jieun: todomain.jsx jan24

--- rootfit_front/src/todo/TodoMain.jsx
+++ rootfit_front/src/todo/TodoMain.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// import Modal from 'react-modal';
+
 import HealthList from './component/HealthList';
 import TodayReport from './component/TodayReport';
 // import MonthReport from './component/MonthReport';
 
 // npm install react-modal 필요! 깃 한다음에 해야지~
 
-import React, { useState, useEffect } from 'react';
-// import Modal from 'react-modal'; //해야하나
-import { Route, Routes } from 'react-router-dom';
+
+// import Modal from 'react-modal'; 
 
 const TodoMain = () => {
     return (
