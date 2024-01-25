dd5a902d9782ad97a63ec8fb143ef880ee858e49
jieun <j@naver.com>
2024-01-25 10:35:33 +0900

    충돌 해결

--- rootfit_front/src/App.jsx
+++ rootfit_front/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import TodoMain from './todo/TodoMain';
 import Header from './home/component/Header'
 import Footer from './home/component/Footer'
 import HomeMain from './home/HomeMain'
+<<<<<<< HEAD
 import ShoppingMain from './shopping/ShoppingMain'
 import UserMain from './user/UserMain';
+=======
+// import ShoppingMain from './shopping/ShoppingMain'
+>>>>>>> d984f94 (App.jsx commit)
 
-import BoardMain from './board/BoardMain'
+// import BoardMain from './board/BoardMain'
 
 const App = () => {
   return (
@@ -21,9 +25,16 @@ const App = () => {
       즉 XXXMain 만 이곳에서 등록하고.. XXX 업무에 의한 화면 라우팅은 XXXMain 에 명시 */}
       <Routes>
         <Route path='/' element={<HomeMain />} />
+<<<<<<< HEAD
         <Route path='/shopping/*' element={<ShoppingMain />} />
         <Route path='/user/*' element={<UserMain />} />
         <Route path='/board/*' element={<BoardMain/>} />
+=======
+        <Route path='/todo/*' element={<TodoMain/>} />
+        {/* <Route path='/shopping/*' element={<ShoppingMain />} /> */}
+        {/* <Route path='/user/*' element={<UserMain />} />
+        <Route path='/board/*' element={<BoardMain/>} /> */}
+>>>>>>> d984f94 (App.jsx commit)
       </Routes>
       <Footer/>
     </div>
