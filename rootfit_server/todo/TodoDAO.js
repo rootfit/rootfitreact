33a9084a5038e22e3bd201ea37f756ae27ebf87d
Bokyung Kwon <tree445560@gmail.com>
2024-01-25 16:27:59 +0900

    kwon: TodoDAO.js, TodoRouter.js 코드를 기획에 맞게 수정.

--- rootfit_server/todo/TodoDAO.js
+++ rootfit_server/todo/TodoDAO.js
@@ -1,27 +1,27 @@
-// const getPool = require('../common/pool');
+const getPool = require('../common/pool');
 
-// const sql = {
-//   // todoreport test용 sql문 간단 예시
-//   healthList: 'SELECT * FROM board',
-// };
+const sql = {
+  // todoreport test용 sql문 간단 예시
+  healthList: 'SELECT * FROM healthlisttbl',
+};
 
-// const todoDAO = {
-//   healthlist: async (callback) => {
-//     let conn = null;
-//     try {
-//       console.log('healthlist try 시작...');
-//       conn = await getPool().getConnection();
-//       const [resp] = await conn.query(sql.healthList, []);
-//       console.log(resp);
-//       console.log('boardlist callback 완료');
-//       callback({ status: 200, message: 'OK', data: resp });
-//     } catch (error) {
-//       console.log(error.message);
-//       return { status: 500, message: '', error: error };
-//     } finally {
-//       if (conn !== null) conn.release();
-//     }
-//   },
-// };
+const todoDAO = {
+  healthlist: async (callback) => {
+    let conn = null;
+    try {
+      console.log('healthlist try 시작...');
+      conn = await getPool().getConnection();
+      const [resp] = await conn.query(sql.healthList, []);
+      console.log(resp);
+      console.log('healthlist callback 완료');
+      callback({ status: 200, message: 'OK', data: resp });
+    } catch (error) {
+      console.log(error.message);
+      return { status: 500, message: 'healthlist callback 실패', error: error };
+    } finally {
+      if (conn !== null) conn.release();
+    }
+  },
+};
 
-// module.exports = todoDAO;
+module.exports = todoDAO;
