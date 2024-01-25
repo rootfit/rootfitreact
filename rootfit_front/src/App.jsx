c153bd8929e6cb67ba1f2b62c7238c8c6fef2147
ham <s1206s@naver.com>
2024-01-25 13:31:28 +0900

    hyen-복구

--- rootfit_front/src/App.jsx
+++ rootfit_front/src/App.jsx
@@ -5,12 +5,10 @@ import TodoMain from './todo/TodoMain';
 import Header from './home/component/Header'
 import Footer from './home/component/Footer'
 import HomeMain from './home/HomeMain'
-<<<<<<< HEAD
+
 import ShoppingMain from './shopping/ShoppingMain'
 import UserMain from './user/UserMain';
-=======
-// import ShoppingMain from './shopping/ShoppingMain'
->>>>>>> d984f94 (App.jsx commit)
+
 
 // import BoardMain from './board/BoardMain'
 
@@ -25,16 +23,9 @@ const App = () => {
       즉 XXXMain 만 이곳에서 등록하고.. XXX 업무에 의한 화면 라우팅은 XXXMain 에 명시 */}
       <Routes>
         <Route path='/' element={<HomeMain />} />
-<<<<<<< HEAD
         <Route path='/shopping/*' element={<ShoppingMain />} />
         <Route path='/user/*' element={<UserMain />} />
         <Route path='/board/*' element={<BoardMain/>} />
-=======
-        <Route path='/todo/*' element={<TodoMain/>} />
-        {/* <Route path='/shopping/*' element={<ShoppingMain />} /> */}
-        {/* <Route path='/user/*' element={<UserMain />} />
-        <Route path='/board/*' element={<BoardMain/>} /> */}
->>>>>>> d984f94 (App.jsx commit)
       </Routes>
       <Footer/>
     </div>
