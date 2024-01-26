e758e46df745e134f82d6b6b93a708b509fcbfd2
Bokyung Kwon <tree445560@gmail.com>
2024-01-26 16:03:20 +0900

    kwon: TodoDAO.js, TodoRouter.js에 저장 함수 추가

--- rootfit_server/todo/TodoDAO.js
+++ rootfit_server/todo/TodoDAO.js
@@ -2,10 +2,14 @@ const getPool = require('../common/pool');
 
 const sql = {
   // todoreport test용 sql문 간단 예시
-  healthList: 'SELECT * FROM healthlisttbl',
+  healthList: 'SELECT * FROM healthlistTBL',
+  healthSelect: 'INSERT INTO userTBL () VALUES (?)',
+  healthSelectInsert:
+    'INSERT INTO healthselectTBL (healthNo, user_id, healthSelect) VALUES (?, ?, ?)',
 };
 
 const todoDAO = {
+  // 유저에게 admin이 작성한 헬스리스트 목록을 보냄
   healthlist: async (callback) => {
     let conn = null;
     try {
@@ -18,6 +22,29 @@ const todoDAO = {
     } catch (error) {
       console.log(error.message);
       return { status: 500, message: 'healthlist callback 실패', error: error };
+    } finally {
+      if (conn !== null) conn.release();
+    }
+  },
+  // 유저가 선택한 헬스리스트를 저장
+
+  // 유저의 누적 데이터를 저장 (작업중)
+  healthselectinsert: async (list, callback) => {
+    let conn = null;
+    try {
+      console.log('healthselectinsert try 시작...');
+      conn = await getPool().getConnection();
+      const [resp] = await conn.query(sql.healthSelectInsert, [
+        list.healthNo,
+        list.user_id,
+        list.healthSelect,
+      ]);
+      console.log(resp);
+      console.log('healthselectinsert callback 완료');
+      callback({ status: 200, message: '헬스리스트가 누적 테이블에 저장되었습니다.', data: resp });
+    } catch (error) {
+      console.log(error.message);
+      return { status: 500, message: 'healthselectinsert callback 실패', error: error };
     } finally {
       if (conn !== null) conn.release();
     }
--- rootfit_server/todo/TodoRouter.js
+++ rootfit_server/todo/TodoRouter.js
@@ -4,9 +4,19 @@ const todoDAO = require('./TodoDAO');
 
 // 유저 요청이 들어왔다.
 
+// 유저에게 admin이 작성한 헬스리스트 목록을 보냄
 router.get('/healthlist', function (req, res, next) {
   console.log('TodoRouter에서 healthlist 요청 확인...');
   todoDAO.healthlist((resp) => {
+    res.json(resp);
+  });
+});
+
+// 유저의 누적 데이터를 저장(작업중)
+router.post('/healthselectinsert', function (req, res, next) {
+  console.log('TodoRouter에서 healthselectinsert 요청 확인...');
+  const data = req.body;
+  todoDAO.healthselectinsert(data, (resp) => {
     res.json(resp);
   });
 });
