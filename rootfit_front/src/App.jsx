30383c5cc4d88289c64c79f4e563dc2ba081545c
Bokyung Kwon <tree445560@gmail.com>
2024-01-26 11:16:48 +0900

    kwon: App.jsx, Header.jsx 에 todo Route와 링크 수정. shoppingMain Route 임시 주석처리.

--- rootfit_front/src/App.jsx
+++ rootfit_front/src/App.jsx
@@ -1,34 +1,35 @@
+// 프레임워크, 라이브러리 등 import
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// 각 업무의 첫화면 import
 import TodoMain from './todo/TodoMain';
-import Header from './home/component/Header'
-import Footer from './home/component/Footer'
-import HomeMain from './home/HomeMain'
-
-import ShoppingMain from './shopping/ShoppingMain'
+import Header from './home/component/Header';
+import Footer from './home/component/Footer';
+import HomeMain from './home/HomeMain';
+import ShoppingMain from './shopping/ShoppingMain';
 import UserMain from './user/UserMain';
-
-
-import BoardMain from './board/BoardMain'
+import BoardMain from './board/BoardMain';
 
 const App = () => {
   return (
     <div>
-      {/* Header, Footer 가 전체 화면에서 항상 고정으로 나온다면.. 아래처럼
-      각 화면별로 Header, Footer 가 상이하게 적용된다면 Route 되는 Component 내에. */}
+      {/* Header, Footer 가 전체 화면에서 항상 고정으로 나온다면.. 아래처럼. */}
       <Header />
       {/* 각 화면이 라우팅 되게 등록...
       각 업무의 첫 화면만 등록하고 그 안에서의 화면전환은 중첩 라우팅으로 처리..
       즉 XXXMain 만 이곳에서 등록하고.. XXX 업무에 의한 화면 라우팅은 XXXMain 에 명시 */}
       <Routes>
         <Route path='/' element={<HomeMain />} />
-        <Route path='/shopping/*' element={<ShoppingMain />} />
+        {/* ShoppingMain은 임시로 주석처리. */}
+        {/* <Route path='/shopping/*' element={<ShoppingMain />} /> */}
         <Route path='/user/*' element={<UserMain />} />
-        <Route path='/board/*' element={<BoardMain/>} />
+        <Route path='/board/*' element={<BoardMain />} />
+        <Route path='/todo/*' element={<TodoMain />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
--- rootfit_front/src/home/component/Header.jsx
+++ rootfit_front/src/home/component/Header.jsx
@@ -1,59 +1,79 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { RiUserAddLine } from "react-icons/ri"
-import { RiUserFill } from "react-icons/ri";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { RiUserAddLine } from 'react-icons/ri';
+import { RiUserFill } from 'react-icons/ri';
 // import { RiUserSettingsLine } from "react-icons/ri";
 
 const Header = () => {
   return (
-    <header id="header" className="header d-flex align-items-center fixed-top">
-      <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
-
-        <a href="/" className="logo d-flex align-items-center">
+    <header id='header' className='header d-flex align-items-center fixed-top'>
+      <div className='container-fluid container-xl d-flex align-items-center justify-content-between'>
+        <a href='/' className='logo d-flex align-items-center'>
           {/* <!-- Uncomment the line below if you also wish to use an image logo --> */}
           {/* <!-- <img src="assets/img/logo.png" alt=""> --> */}
           <h1>Root Fit</h1>
         </a>
 
-        <nav id="navbar" className="navbar">
+        <nav id='navbar' className='navbar'>
           <ul>
-            <li><Link className="nav-link " to="/shopping/product">쇼핑몰</Link></li>
-            <li><a href="index.html">투두리스트</a></li>
-            <li><a href="single-post.html">게시판</a></li>
-            <li><a href="/shopping">쇼핑몰</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li>
+              <Link className='nav-link ' to='/shopping/product'>
+                쇼핑몰
+              </Link>
+            </li>
+            <li>
+              <Link className='nav-link' to='/todo'>
+                헬스리스트
+              </Link>
+            </li>
+            <li>
+              <a href='single-post.html'>게시판</a>
+            </li>
+            <li>
+              <a href='/shopping'>쇼핑몰</a>
+            </li>
+            <li>
+              <a href='about.html'>About</a>
+            </li>
+            <li>
+              <a href='contact.html'>Contact</a>
+            </li>
           </ul>
         </nav>
         {/* <!-- .navbar --> */}
 
-        <div className="position-relative">
+        <div className='position-relative'>
           <RiUserAddLine className='icon' size='20' color='black' />
-          <Link className="mx-2" to="/user/signup">SIGN UP</Link>
+          <Link className='mx-2' to='/user/signup'>
+            SIGN UP
+          </Link>
           <RiUserFill className='icon' size='20' color='black' />
-          <Link className="mx-2" to="/user/signin">SIGN IN</Link>
+          <Link className='mx-2' to='/user/signin'>
+            SIGN IN
+          </Link>
           {/* <RiUserSettingsLine className='icon' size='20' color='black' />
           <Link className="mx-2" to="/">MODIFY</Link> */}
 
-          <a href="#" className="mx-2 js-search-open"><span className="bi-search"></span></a>
-          <i className="bi bi-list mobile-nav-toggle"></i>
+          <a href='#' className='mx-2 js-search-open'>
+            <span className='bi-search'></span>
+          </a>
+          <i className='bi bi-list mobile-nav-toggle'></i>
 
           {/* <!-- ======= Search Form ======= --> */}
-          <div className="search-form-wrap js-search-form-wrap">
-            <form action="search-result.html" className="search-form">
-              <span className="icon bi-search"></span>
-              <input type="text" placeholder="Search" className="form-control" />
-              <button className="btn js-search-close"><span className="bi-x"></span></button>
+          <div className='search-form-wrap js-search-form-wrap'>
+            <form action='search-result.html' className='search-form'>
+              <span className='icon bi-search'></span>
+              <input type='text' placeholder='Search' className='form-control' />
+              <button className='btn js-search-close'>
+                <span className='bi-x'></span>
+              </button>
             </form>
           </div>
           {/* <!-- End Search Form --> */}
-
         </div>
-
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
