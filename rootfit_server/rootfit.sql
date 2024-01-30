017a5ac8d8b3fac3d02f55c6b414867bb1068c9e
Seob <mp200002@naver.com>
2024-01-30 11:39:42 +0900

    sql update

--- rootfit_server/rootfit.sql
+++ rootfit_server/rootfit.sql
@@ -1,4 +1,62 @@
 
+-- 회원테이블
+CREATE TABLE IF NOT EXISTS userTBL(
+     id VARCHAR(20) NOT NULL,
+     password VARCHAR(200) NOT NULL,
+     nickname VARCHAR(10) NOT NULL,
+     phone VARCHAR(20) NOT NULL,
+     email VARCHAR(50) NOT NULL,
+     addr VARCHAR(100),
+     createAt DATETIME DEFAULT now(),
+     isAdmin Boolean NOT NULL DEFAULT false,
+     healthSelect JSON,
+     PRIMARY KEY (id)
+);
+
+-- 게시글 테이블
+CREATE TABLE IF NOT EXISTS boardTBL(
+     id INT NOT NULL AUTO_INCREMENT,
+     user_id VARCHAR(20) NOT NULL,
+     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+     title VARCHAR(100) NOT NULL,
+     content TEXT NOT NULL,
+     cnt INT DEFAULT '0',
+     reccnt INT DEFAULT '0',
+     img VARCHAR(50),
+     CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCEs userTBL(id),
+     PRIMARY KEY (id)
+);
+
+-- 댓글테이블 
+CREATE TABLE IF NOT EXISTS commentTBL(
+     id INT NOT NULL AUTO_INCREMENT,
+     user_id VARCHAR(20) NOT NULL,
+     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+     board_id INT NOT NULL,
+     content varchar(100) NOT NULL,
+     CONSTRAINT fk_comment_user_id FOREIGN KEY(user_id) REFERENCES userTBL(id),
+     CONSTRAINT fk_comment_board_id FOREIGN KEY(board_id) REFERENCES boardTBL(id),
+     PRIMARY KEY (id)
+);
+
+-- Do Health List 테이블
+CREATE TABLE IF NOT EXISTS healthlistTBL(
+     healthNo VARCHAR(200) NOT NULL,
+     healthTitle VARCHAR(20) NOT NULL,
+     healthContent VARCHAR(100) NOT NULL,
+     PRIMARY KEY (healthNo)
+);
+
+-- Do Health List 누적 테이블
+CREATE TABLE IF NOT EXISTS healthselectTBL(
+     healthNo VARCHAR NOT NULL,
+     user_id VARCHAR(20),
+     createAt DATE  NOT NULL DEFAULT now(),
+     healthSelect JSON NOT NULL,
+     CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCE userTBL(id),
+     PRIMARY KEY (healthNo)
+);
+
 -- 장바구니 테이블
 CREATE TABLE IF NOT EXISTS cartTbl(
      cartNum INT NOT NULL AUTO_INCREMENT,
