f5351714741ebe6af8b2b9bdcbfa12a7de253c8b
ham <s1206s@naver.com>
2024-02-01 10:46:14 +0900

    hyen-yy.mm.dd로 수정

--- rootfit_front/src/board/component/BoardList.jsx
+++ rootfit_front/src/board/component/BoardList.jsx
@@ -8,10 +8,10 @@ const BoardList = () => {
   //생성일이 년, 월, 일만 나오도록하는 로직
   const CreatedAt = (createdAt) => {
     const date = new Date(createdAt);
-    const year = date.getFullYear();
+    const year = date.getFullYear().toString().slice(-2);
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    return `${year}.${month}.${day}`;
   };
   // statusHandler 만들기
   // 다룰 데이터 명시
