9eb3a777b437e4395ffcf1b1dfcb75e83ff28b7d
Bokyung Kwon <tree445560@gmail.com>
2024-02-02 11:34:33 +0900

    kwon: 헬스리스트 메인 화면에 유저 데이터가 출력되도록 임시 작업 진행.

--- rootfit_front/src/todo/component/HealthList.jsx
+++ rootfit_front/src/todo/component/HealthList.jsx
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import HealthModal from './HealthModal';
+import axios from 'axios';
 
 const HealthList = () => {
   const [tasks, setTasks] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [checkboxState, setCheckboxState] = useState(false);
+  const [checkboxState, setCheckboxState] = useState([]);
+  const [loadList, setLoadList] = useState([]);
+
+  // 회원 데이터를 요청하는 함수
+  const getLoadList = useCallback(async () => {
+    console.log('111');
+    const resp = await axios.get('http://localhost:8000/todo/loadlist');
+    console.log('resp', resp.data.data);
+    setLoadList(resp.data.data);
+  }, []);
 
   // 모달 열기
   const openModal = () => {
@@ -25,8 +35,9 @@ const HealthList = () => {
 
   // 현재 날짜 정보 가져오기
   const currentDate = new Date();
-  const formattedDate = `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1
-    }월 ${currentDate.getDate()}일`;
+  const formattedDate = `${currentDate.getFullYear()}년 ${
+    currentDate.getMonth() + 1
+  }월 ${currentDate.getDate()}일`;
 
   // 헬스리스트 추가 함수
   const addHealthList = (newHealthList) => {
@@ -41,7 +52,9 @@ const HealthList = () => {
       now.getFullYear(),
       now.getMonth(),
       now.getDate() + 1, // 다음 날 자정
-      0, 0, 0
+      0,
+      0,
+      0
     );
 
     const timeUntilMidnight = midnight - now;
@@ -63,24 +76,51 @@ const HealthList = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  useState(() => {
+    getLoadList();
+  }, [loadList]);
 
   return (
     <div className='container mt-5'>
       {/* 페이지 제목 */}
-      <h1 className="title-single">Health List</h1>
-      <span className="color-text-a">💪 나만의 헬스리스트 🏋️‍♂️</span>
+      <h1 className='title-single'>Health List</h1>
+      <span className='color-text-a'>💪 나만의 헬스리스트 🏋️‍♂️</span>
       <h2 className='text-center mb-4'>건강한 일상을 가꾸는 소소한 루틴</h2>
       <h3 className='text-center mb-4'>HealthList를 추가하고 매일 루틴을 체크해봐요💫</h3>
 
       {/* 현재 날짜 표시 */}
       <p className='text-center mb-4'>{formattedDate}</p>
 
+      {/* 헬스리스트 목록 */}
+      <ul className='list-group mt-3'>
+        {loadList.map((task, index) => (
+          <li
+            key={index}
+            className='list-group-item d-flex justify-content-between align-items-center'
+          >
+            {/* 태스크 내용 */}
+            {task}
+            {/* 체크박스 */}
+            <input
+              type='checkbox'
+              // checked='!checked'
+              // onChange={() => {
+              //   // 체크박스 상태 변경 처리
+              //   const updatedTasks = [...tasks];
+              //   updatedTasks[index].checked = !task.checked;
+              //   setTasks(updatedTasks);
+              // }}
+              className='mx-2'
+            />
+          </li>
+        ))}
+      </ul>
+
       <div className='d-flex justify-content-center mb-5'>
         {/* 새로운 헬스리스트 추가 버튼 */}
-
         <button
-          type="button"
-          className="btn btn-dark m-1 col-2"
+          type='button'
+          className='btn btn-dark m-1 col-2'
           style={{ height: '50px', fontWeight: 'bold' }}
           onClick={openModal}
         >
@@ -95,38 +135,14 @@ const HealthList = () => {
           setCheckboxState={setCheckboxState}
           addHealthList={addHealthList} // 모달에서 추가된 헬스리스트를 메인 창에 전달
         />
-        {/* 헬스리스트 목록 */}
-        <ul className='list-group mt-3'>
-          {tasks.map((task, index) => (
-            <li
-              key={index}
-              className='list-group-item d-flex justify-content-between align-items-center'
-            >
-              {/* 태스크 내용 */}
-              {task.task}
-              {/* 체크박스 */}
-              <input
-                type='checkbox'
-                checked={task.checked}
-                onChange={() => {
-                  // 체크박스 상태 변경 처리
-                  const updatedTasks = [...tasks];
-                  updatedTasks[index].checked = !task.checked;
-                  setTasks(updatedTasks);
-                }}
-                className='mx-2'
-              />
-              {/* 삭제 버튼 */}
-              <button onClick={() => deleteTask(index)} className='btn btn-danger'>
-                삭제
-              </button>
-            </li>
-          ))}
-        </ul>
+
+        {/* 삭제 버튼 */}
+        <button onClick={() => deleteTask(index)} className='btn btn-danger'>
+          삭제
+        </button>
       </div>
     </div>
   );
 };
 
 export default HealthList;
-
