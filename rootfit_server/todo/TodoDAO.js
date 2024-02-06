082d3b9ca0d563ee4f8f7301e3088024e1353dba
Bokyung Kwon <tree445560@gmail.com>
2024-02-06 13:36:38 +0900

    kwon: TodoRouter.js에서 누적 데이터 저장 기능 구현 완료.

--- rootfit_server/todo/TodoDAO.js
+++ rootfit_server/todo/TodoDAO.js
@@ -4,27 +4,24 @@ const sql = {
   // 헬스리스트 목록 불러오기
   healthList: 'SELECT * FROM healthlistTBL;',
 
-  // 테스트용 누적 데이터 insert (현재 이걸로 사용)
+  // 누적 데이터 최초 저장하기
   insertSelect: 'INSERT INTO healthselectTBL (healthNo, user_id, healthSelect) VALUES (?, ?, ?);',
-  // 실사용 누적 데이터 insert (사용X)
-  // healthSelectInsert:
-  //   'INSERT INTO healthselectTBL (user_id, healthSelect) VALUES (?, ?)',
 
-  // 테스트용 유저 누적 데이터 불러오기
+  // ✨테스트용✨ 당일 유저 누적 데이터 전체 불러오기
   loadAllSelect: 'SELECT * FROM healthselectTBL WHERE user_id = ?;',
-  // 실사용 누적 데이터 불러오기
-  // selectList: 'SELECT * FROM healthselectTBL WHERE id = ? AND datediff(createAT, now()) = 0;',
+  // ✨실사용✨ 당일 누적 데이터 전체 불러오기
+  // loadAllSelect: 'SELECT * FROM healthselectTBL WHERE id = ? AND datediff(createAT, now()) = 0;',
 
-  // 테스트용 유저 누적 데이터 중 healthSelect만 불러오기
-  // loadList: 'SELECT healthSelect FROM healthselectTBL WHERE user_id = ?;',
-  // 실사용 유저 누적 데이터 중 healthSelect만 불러오기
+  // ✨테스트용✨ 유저 누적 데이터 중 healthSelect만 불러오기
+  // loadOnlySelect: 'SELECT healthSelect FROM healthselectTBL WHERE user_id = ?;',
+  // ✨실사용✨ 유저 누적 데이터 중 healthSelect만 불러오기
   loadOnlySelect:
     'SELECT healthSelect FROM healthselectTBL WHERE user_id = ? AND datediff(createAT, now()) = 0;',
 
-  // 테스트용 유저 누적 데이터 update
+  // ✨테스트용✨ 유저 누적 데이터 update
   updateOnlySelect: 'UPDATE healthselectTBL SET healthSelect = ? WHERE user_id = ?;',
-  // 실사용 유저 누적 데이터 update
-  // healthSelect: 'UPDATE healthselectTBL SET healthSelect = ? WHERE user_id = ? AND datediff(createAT, now()) = 0;',
+  // ✨실사용✨ 유저 누적 데이터 update
+  // updateOnlySelect: 'UPDATE healthselectTBL SET healthSelect = ? WHERE user_id = ? AND datediff(createAT, now()) = 0;',
 };
 
 const todoDAO = {
@@ -92,23 +89,23 @@ const todoDAO = {
 
   // 유저가 모달창에서 선택해서 저장한 데이터를 누적 데이터 db에 저장
   insertselect: async (data, callback) => {
-    const jsonData = JSON.stringify(data);
-    console.log('jsonData', jsonData);
-    const user_id = jsonData['id'];
-    console.log('id', jsonData['id']);
-    const columnData = 's' + user_id;
-    delete jsonData.id;
-    const jsonList = { healthNo: columnData, user_id: user_id, healthSelect: jsonData }; // healthNo와 user_id는 임시값. healthNo는 수동으로 계속 바꿔줘야 함.
     let conn = null;
     try {
       conn = await getPool().getConnection();
+      const user_id = data.id;
+      delete data.id;
+      const jsonData = JSON.stringify(data);
+      const todayDate = `${Date.now()}`;
+      const columnData = 's' + user_id + todayDate;
+      const jsonList = { healthNo: columnData, user_id: user_id, healthSelect: jsonData };
+      console.log('jsonList', jsonList);
       const [resp] = await conn.query(sql.insertSelect, [
         jsonList.healthNo,
         jsonList.user_id,
         jsonList.healthSelect,
       ]);
       console.log('resp', resp);
-      // callback({ status: 200, message: '헬스리스트가 누적 테이블에 저장되었습니다.' });
+      callback({ status: 200, message: '헬스리스트가 누적 테이블에 저장되었습니다.' });
       console.log('insertselect callback 완료');
     } catch (error) {
       console.log(error.message);
