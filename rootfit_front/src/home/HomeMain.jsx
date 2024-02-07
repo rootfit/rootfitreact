0803d148d40a79928b86343e623f7b48810ac400
ham <s1206s@naver.com>
2024-02-07 17:09:23 +0900

    hyen-메인홈 수정

--- rootfit_front/src/home/HomeMain.jsx
+++ rootfit_front/src/home/HomeMain.jsx
@@ -12,16 +12,15 @@ const HomeMain = () => {
       {/* <div className="col-12">
       <투두함수 />
       </div> */}
-      {/* <div className="col-6">
-      <쇼핑 />
-      </div> */}
-      <div className="col-6">
-        <RecentBoardList />
-      </div>
+      <div className="row">
 
-      {/* 의미 없는 컴포넌트가 선언된 것처럼 보이지만..
-      무언가 상황에 따라.홈 화면을 위한 컴포넌트가 더 추가될 수도 있고..
-      홈 화면내에 충첩 라우팅으로.. 여러 화면이 변경되면서 나올수도 있어서 유지한다. */}
+        <div className="col-6">
+          {/* <쇼핑 /> */}
+        </div>
+        <div className="col-6">
+          <RecentBoardList />
+        </div>
+      </div>
     </div>
   )
 }
