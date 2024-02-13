a2c3dc85faaa6e5e1d277e0591dfdac4dc6e3fdb
jieun <j@naver.com>
2024-02-13 16:37:25 +0900

    jieun : home css 화면에 아이콘 추가

--- rootfit_front/src/home/component/todo.jsx
+++ rootfit_front/src/home/component/todo.jsx
@@ -1,11 +1,12 @@
 // src/home/component/todo.jsx
 
-import React, { useState, useEffect , useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // useNavigate import 추가
 import axios from 'axios';
 import CheckboxList from '../../todo/component/CheckboxList';
 
 import UserContext from '../../user/context/UserContext'
+import iconUrl from './icon/icons8-체크리스트.gif';
 
 const Todo = () => {
     const navigate = useNavigate();
@@ -17,7 +18,7 @@ const Todo = () => {
     useEffect(() => {
         const fetchHealthList = async () => {
             try {
-                const resp = await axios.get('http://localhost:8000/todo/loadselect/'+userInfo.state.user.id);
+                const resp = await axios.get('http://localhost:8000/todo/loadselect/' + userInfo.state.user.id);
                 setHealthList(resp.data.data);
             } catch (error) {
                 console.error('Error fetching health list:', error);
@@ -37,7 +38,12 @@ const Todo = () => {
         <div className='container'>
             <br />
             <br />
-            <h3 className='center' type="button" onClick={() => navigate('/todo')}>💪 나만의 헬스리스트 🏋️‍♂️</h3>
+            {/* <a target="_blank" href="https://icons8.com/icon/RFI53ZLVF5Ga/%EC%B2%B4%ED%81%AC%EB%A6%AC%EC%8A%A4%ED%8A%B8">체크리스트</a> 작가: <a target="_blank" href="https://icons8.com">Icons8</a> */}
+            <h3 className='center' type="button" onClick={() => navigate('/todo')}>
+                <span style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={iconUrl} alt="Icon" style={{ marginRight: '20px' }} />
+                    헬스리스트                </span>
+                </h3>
             <span className='center'>건강한 삶의 시작은 여기서부터!</span>
             <br />
             <br />
