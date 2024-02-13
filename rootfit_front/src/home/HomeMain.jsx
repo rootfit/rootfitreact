3dce748eca9a61f743a8490e566d6cc677864740
Seob <mp200002@naver.com>
2024-02-13 17:45:42 +0900

    2.13 shopUpdate

--- rootfit_front/src/home/HomeMain.jsx
+++ rootfit_front/src/home/HomeMain.jsx
@@ -1,43 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import Home from "./component/Home";
 import RecentBoardList from "./component/board"
 import RecentProductList from "./component/shopping"
 // import 쇼핑어쩌구
 import Health from "./component/todo";
+import iconUrl from './component/icon/rootfit_logo.png'; // 경로를 프로젝트 구조에 맞게 수정
 
 const HomeMain = () => {
   // const 투두랜덤함수 = () => {
   //   // 아이디 비교 로직( 보드디테일 참고 가능~! )
+
+  //메인 큰 로고 CSS적용
+  const [shouldAnimate, setShouldAnimate] = useState(true);
+  useEffect(() => {
+    // 페이지 로드 후 1초 후에 애니메이션 종료
+    const timer = setTimeout(() => {
+      setShouldAnimate(false);
+    }, 1000);
+    // 컴포넌트 언마운트 시 타이머 클리어
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="container">
-      {/* <Home /> */}
       <div className="row">
-        <div>
-          <h1 className="text-center">
-            <br />
-            {/* 로고 만들어서 대체하는게 좋을 것 같아요~ */}
-            RootFit
-            <br />
-          </h1>
-          <br />
-          <hr className="hr-solid"/ >
+        <div className={`rootfit-container text-center ${shouldAnimate ? 'animate' : ''}`}>
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={iconUrl} alt="Icon" className="image" />
+            </span>
         </div>
+        <hr className="hr-solid" />
 
-        
+
         {/* todo */}
         <div>
           <Health />
           {/* todo.jsx에 위아래 여백 더 주세요 */}
         </div>
-        <hr className="hr-solid"/ >
-        
-        
+        <hr className="hr-solid" />
+
+
         {/* shop */}
         <div>
           <RecentProductList />
         </div>
-        <hr className="hr-solid"/ >
-        
-        
+        <hr className="hr-solid" />
+
+
         {/* board */}
         <div>
           <RecentBoardList />
