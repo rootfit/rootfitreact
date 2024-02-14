296e5a4edc76592cb1f166a014a16fbc66ac99f9
minsuh <minsuh@kakao.com>
2024-02-14 14:44:46 +0900

    Kang: User_회원가입, 로그인, 회원정보 수정 개발 구현 완료

--- rootfit_front/src/board/component/CommentsList.jsx
+++ rootfit_front/src/board/component/CommentsList.jsx
@@ -1,6 +1,8 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import React, { useCallback, useState, useEffect } from 'react'
+// import ReactHtmlParser from 'react-html-parser';
+
 // import Cookies from 'js-cookie'
 
 const CommentsList = () => {
@@ -71,57 +73,91 @@ const CommentsList = () => {
     setInputComment('')
   }, [inputComment, getComments, id])
 
+// 삭제버튼 클릭시에
+  const deleteComment =  async (id) => {
+    //버튼 클릭시에 호출되어 서버에 매개변수 데이터 삭제되게 요청.
+    try {
+      // 서버에 DELETE 요청을 보냄
+      await axios.get(`http://localhost:8000/board/deletecomment/${id}`);
+      // 댓글 목록 다시 불러오기
+      getComments();
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+    }
+  };
+
+
 
   // id에 따른 버튼 보기
-  
+  const commentDeleteButton = (id, user_id) => {
+    // 로그인 아이디와 댓글의 유저아이디 비교
+    if (loggedInUserId === user_id) {
+      return (
+        <div>
+            <button type="button" className="btn btn-end" onClick={() => deleteComment(id)}>
+              삭제
+            </button>
+          </div>
+      );
+    } else {
+      return (
+        <div>
+        </div>
+      );
+    }
+  }
 
   // 생명주기 hook
   useEffect(() => {
     fetchLoggedInUserId();
     getComments();
-  }, [id, getComments, addComment])
+    deleteComment();
+  }, [id, getComments])
 
 
   return (
     <main id="commentsmain">
       <div className='container'>
 
-      {/* 댓글 입력 form */}
-      <section>
-        <div className='container'>
-          <h4>comment</h4>
-          <hr className="hr-solid"/ >
-          <br />
+        {/* 댓글 입력 form */}
+        <section>
+          <div className='container'>
+            <h4>comment</h4>
+            <hr className="hr-solid" />
+            <br />
             <div className="row">
-            <div className="col-lg-11 col-md-10 col-sm-10 mb-3">
+              <div className="col-lg-11 col-md-10 col-sm-10 mb-3">
                 <textarea className="form-control" id="comment-message" placeholder="댓글을 입력하세요." rows="1" value={inputComment} onChange={(e) => setInputComment(e.target.value)}>
                 </textarea>
               </div>
               <div className="col-lg-1 col-md-2 col-sm-2 text-end">
                 <input type="submit" className="btn btn-primary " value="입력" onClick={addComment} />
               </div>
-            {/* </div> */}
+              {/* </div> */}
+            </div>
           </div>
-        </div>
-        {/* <!-- End Comments Form --> */}
+          {/* <!-- End Comments Form --> */}
 
-        {/* <!-- ======= 댓글창 ======= --> */}
-        <div className='container'>
-          <table className='table'>
-            <tbody>
-              {comment.map((contents) => (
-                <tr key={contents.id}>
-                  <td>{contents.nickname}</td>
-                  <td>{contents.content}</td>
-                  <td className='text-end'>{CreatedAt(contents.createdAt)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          {/* <!-- ======= 댓글창 ======= --> */}
+          <div className='container'>
+            <table className='table'>
+              <tbody>
+                
+                {comment.map((contents) => (
+                  <tr key={contents.id}>
+                    <td>{contents.nickname}</td>
+                    <td style={{ whiteSpace: 'pre-line' }}>{contents.content}</td>
+                    <td className='text-end'>{CreatedAt(contents.createdAt)}{commentDeleteButton(contents.id, contents.user_id)}</td>
+                    
+                  </tr>
+                ))}
+                
+              </tbody>
+            </table>
 
-        </div>
-        {/* <!-- End Comments --> */}
-      </section>
+          </div>
+          {/* <!-- End Comments --> */}
+        </section>
       </div>
     </main >
   )
