c28fd829433034115f1ecd7d040a6c9ac755b9bf
Bokyung Kwon <tree445560@gmail.com>
2024-12-02 17:18:32 +0900

    Edit: [H1] kwon - 모든 페이지에서 필요한 컴포넌트를 하나로 묶어 간편하게 출력 되도록 수정

--- rootfit_front/src/App.jsx
+++ rootfit_front/src/App.jsx
@@ -1,16 +1,16 @@
 // 프레임워크, 라이브러리 등 import
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
 // 각 업무의 첫화면 import
-import TodoMain from './todo/sections/view/TodoMain';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeMain from './pages/HomeMain';
+import MainPage from './pages/MainPage';
+import TodoMain from './todo/sections/view/TodoMain';
+import BoardMain from './pages/BoardMain';
 import ShoppingMain from './pages/ShoppingMain';
 import UserMain from './user/UserMain';
-import BoardMain from './pages/BoardMain';
 
 import MemberPage from './user/pages/MemberPage';
 import LogoutPage from './user/pages/LogoutPage';
@@ -22,28 +22,40 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
+const MainLayout = () => {
+  return (
+    <div>
+      <Header />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <TodoProvider>
-          <div>
-            <Header />
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <TodoProvider>
             <Routes>
-              <Route path='/' element={<HomeMain />} />
-              <Route path='/shopping/*' element={<ShoppingMain />} />
-              <Route path='/user/*' element={<UserMain />} />
-              <Route path='/board/*' element={<BoardMain />} />
-              <Route path='/todo/*' element={<TodoMain />} />
-              <Route path='/member' element={<MemberPage />} />
-              <Route path='/logout' element={<LogoutPage />} />
+              <Route path='/' element={<MainLayout />}>
+                <Route index element={<MainPage />} />
+                <Route path='/shopping/*' element={<ShoppingMain />} />
+                <Route path='/user/*' element={<UserMain />} />
+                <Route path='/board/*' element={<BoardMain />} />
+                <Route path='/todo/*' element={<TodoMain />} />
+                <Route path='/member' element={<MemberPage />} />
+                <Route path='/logout' element={<LogoutPage />} />
+              </Route>
             </Routes>
-            <Footer />
-          </div>
-        </TodoProvider>
-      </UserProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+          </TodoProvider>
+        </UserProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </div>
   );
 };
 export default App;
